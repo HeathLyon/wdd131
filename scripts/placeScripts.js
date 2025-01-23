b7ad905e57f1d4ec1e2acf5499517483bcb2f964
Heathyman <lyon.m.heath@gmail.com>
2025-01-22 18:31:41 -0700

    weather

--- scripts/placeScripts.js
+++ scripts/placeScripts.js
@@ -1,3 +1,13 @@
+const temp = 8;
+const windSpeed = 15;
+
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const calculateWindChill = (temp, windSpeed) => 
+    13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+  
+
+  window.onload = () => document.getElementById("wind-chill").textContent = 
+    (temp <= 10 && windSpeed > 4.8) ? `Wind Chill: ${calculateWindChill(temp, windSpeed).toFixed(2)} °C` : "Wind Chill: N/A";
