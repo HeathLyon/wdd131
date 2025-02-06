99c94ff00c69015f4488460387344d4317ecb95e
Heathyman <lyon.m.heath@gmail.com>
2025-02-05 19:10:44 -0700

    siteplanWork?

---
+++ project/scripts/scripts.js
@@ -0,0 +1,10 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const menuButton = document.getElementById('menuButton');
+const menuItems = document.getElementById('menuItems');
+
+menuButton.addEventListener('click', () => {
+    menuItems.classList.toggle('open');
+});
